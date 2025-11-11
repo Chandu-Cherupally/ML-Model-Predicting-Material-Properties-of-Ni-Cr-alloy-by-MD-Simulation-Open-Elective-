7cfbb7c93e6b1e67a6ba06c6f0c2a4165c13d0a7
Chandu Cherupally <146619260+Chandu-Cherupally@users.noreply.github.com>
2025-11-11 22:59:38 +0530

    Add files via upload

---
+++ scripts/Visualization/__init__.py
@@ -0,0 +1,8 @@
+# // 22CSB0C20 CHANDU CHERUPALLY
+"""
+scripts.visualization package
+Exposes a convenience function `generate_all_plots` which calls submodule functions.
+"""
+from .plot_results import generate_all_plots
+
+__all__ = ["generate_all_plots"]
